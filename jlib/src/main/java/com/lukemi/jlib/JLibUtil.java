ad9fe3689c822adac056613707498075141fb290
chenmingzhi <chenmingzhi@ccclubs.com>
2019-09-18 12:36:29 +0800

    add

---
+++ jlib/src/main/java/com/lukemi/jlib/JLibUtil.java
@@ -0,0 +1,15 @@
+package com.lukemi.jlib;
+
+public class JLibUtil {
+
+    /**
+     * 求和
+     *
+     * @param a 参数1
+     * @param b 参数2
+     * @return
+     */
+    public static final int sum(int a, int b) {
+        return a + b;
+    }
+}
